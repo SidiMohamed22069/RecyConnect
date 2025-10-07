24574d8c6820c9e7cdfff4f066a33208d3cdbaea
nextry2025 <nextry22053@gmail.com>
2025-10-07 14:29:32 +0200

    creation du projet

---
+++ backend/src/main/java/com/project/RecyConnect/RecyConnectApplication.java
@@ -0,0 +1,13 @@
+package com.project.RecyConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyConnectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/RecyConnect/RecyConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.RecyConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
